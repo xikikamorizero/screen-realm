4c9a9670f4ed7fefe3c464503bf4ca9b7b8db750
Kogay Artem <kogay10987@gmail.com>
2023-06-09 16:07:12 +0500

    first

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './styles/color.css';
+import './styles/index.css';
+
+
+const App = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default App;

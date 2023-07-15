3076581d5dd1725a3277a333155ebc6971311343
Kogay Artem <kogay10987@gmail.com>
2023-07-15 22:02:05 +0500

    no update

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,25 +1,14 @@
 import React, { useContext } from "react";
 import "./styles/index.css";
 import "./styles/colors.css";
+import { CheckAccountAccess } from "../processes";
 import { observer } from "mobx-react-lite";
-import { Route, Routes } from "react-router-dom";
 import { protected_routers, public_routers } from "./routers";
 import { Container } from "../shared/components";
 import { MainHeader } from "../entities";
 import styled from "styled-components";
 import { MainFooter } from "../entities";
 import { Context } from "../shared/api";
-import {
-  Main,
-  Top100FilmList,
-  Top250Films,
-  Recommendations,
-  SearchFilms,
-  AboutUs,
-  BookmarksPage,
-  Catalog,
-} from "../pages";
-import {FilmInfoBlock} from '../widgets';
 
 const App = observer(() => {
   const { store } = useContext(Context);
@@ -32,17 +21,10 @@ const App = observer(() => {
           store.error == 402 ? { maxHeight: "93vh", overflow: "hidden" } : {}
         }
       >
-          <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/films/*' element={<FilmInfoBlock />} />
-          <Route path='/popular_films' element={<Top100FilmList />} />
-          <Route path='/best_films' element={<Top250Films />} />
-          <Route path='/recommendations' element={<Recommendations />} />
-          <Route path='/search' element={<SearchFilms />} />
-          <Route path='/aboutUs' element={<AboutUs />} />
-          <Route path='/bookmarks' element={<BookmarksPage />} />
-          <Route path='/catalog' element={<Catalog />} />
-        </Routes>
+        <CheckAccountAccess
+          protectedRoutes={protected_routers}
+          publicRoutes={public_routers}
+        />
       </Center>
       <MainFooter />
     </AppContainer>

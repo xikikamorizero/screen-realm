56768bffade659354be6d3b068c7ea20923a0910
Kogay Artem <kogay10987@gmail.com>
2023-08-29 21:15:47 +0500

    fix navbar

--- src/entities/MainHeader/components/BurgerMenu.tsx
+++ src/entities/MainHeader/components/BurgerMenu.tsx
@@ -5,162 +5,162 @@ import search from "../assets/search.png";
 import exit from "../assets/exit.svg";
 
 type PropsType = {
-  click: (a: boolean) => void;
-  burger: boolean;
+    click: (a: boolean) => void;
+    burger: boolean;
 };
 
 export const Burger = ({ ...props }: PropsType) => {
-  let navigate = useNavigate();
-  return (
-    <Container
-      open={props.burger}
-      onClick={(e) => {
-        props.click(false);
-      }}
-    >
-      <BurgerContainer
-        open={props.burger}
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
-      >
-        <HeaderBurger>
-          <Icon
-            width={"40px"}
-            height={"40px"}
-            iconSize={"40px"}
-            icon={search}
-            onClick={() => {
-              navigate("/search");
-              props.click(false);
+    let navigate = useNavigate();
+    return (
+        <Container
+            open={props.burger}
+            onClick={(e) => {
+                props.click(false);
             }}
-          />
-               <Icon
-            width={"40px"}
-            height={"40px"}
-            iconSize={"40px"}
-            icon={exit}
-            onClick={() => {
-            props.click(false)
-            }}
-          />
-        </HeaderBurger>
+        >
+            <BurgerContainer
+                open={props.burger}
+                onClick={(e) => {
+                    e.stopPropagation();
+                }}
+            >
+                <HeaderBurger>
+                    <Icon
+                        width={"40px"}
+                        height={"40px"}
+                        iconSize={"40px"}
+                        icon={search}
+                        onClick={() => {
+                            navigate("/search");
+                            props.click(false);
+                        }}
+                    />
+                    <Icon
+                        width={"40px"}
+                        height={"40px"}
+                        iconSize={"40px"}
+                        icon={exit}
+                        onClick={() => {
+                            props.click(false);
+                        }}
+                    />
+                </HeaderBurger>
 
-        <TitleNavbar
-          onClick={() => {
-            navigate("/catalog")
-            props.click(false)
-          }}
-        >
-          Каталог
-        </TitleNavbar>
-        <TitleNavbar
-          onClick={() => {
-            navigate("/bookmarks")
-            props.click(false)
-          }}
-        >
-          Закладки
-        </TitleNavbar>
-        <TitleNavbar
-          onClick={() => {
-            navigate("/aboutUs")
-            props.click(false)
-          }}
-        >
-          О нас
-        </TitleNavbar>
-      </BurgerContainer>
-    </Container>
-  );
+                <TitleNavbar
+                    onClick={() => {
+                        navigate("/catalog");
+                        props.click(false);
+                    }}
+                >
+                    Каталог
+                </TitleNavbar>
+                <TitleNavbar
+                    onClick={() => {
+                        navigate("/bookmarks");
+                        props.click(false);
+                    }}
+                >
+                    Закладки
+                </TitleNavbar>
+                <TitleNavbar
+                    onClick={() => {
+                        navigate("/aboutUs");
+                        props.click(false);
+                    }}
+                >
+                    О нас
+                </TitleNavbar>
+            </BurgerContainer>
+        </Container>
+    );
 };
 
 type StyleType = {
-  open: boolean;
+    open: boolean;
 };
 
 const Container = styled.div`
-  position: fixed;
-  right: 0;
-  top: 0;
-  height: 100vh;
-  width: 100%;
-  z-index: 11;
-  display: flex;
-  background-color: rgba(0, 0, 0, 0.85);
-  justify-content: end;
-  overflow-x: hidden;
-  pointer-events: ${({ open }: StyleType) => (open ? "all" : "none")};
-  transition: all 0.3s;
-  opacity: ${({ open }: StyleType) => (open ? 1 : 0)};
+    position: fixed;
+    right: 0;
+    top: 0;
+    height: 100vh;
+    width: 100%;
+    z-index: 11;
+    display: flex;
+    background-color: rgba(0, 0, 0, 0.85);
+    justify-content: end;
+    overflow-x: hidden;
+    pointer-events: ${({ open }: StyleType) => (open ? "all" : "none")};
+    transition: all 0.3s;
+    opacity: ${({ open }: StyleType) => (open ? 1 : 0)};
 `;
 const BurgerContainer = styled.div`
-  position: relative;
-  width: ${({ open }: StyleType) => (open ? "500px" : "0")};
-  height: 100vh;
-  display: flex;
-  gap: 20px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: var(--main);
-  overflow-x: hidden;
-  overflow-y: scroll;
-  transition: all 0.3s;
-  padding-bottom: 90px;
+    position: relative;
+    width: ${({ open }: StyleType) => (open ? "500px" : "0")};
+    height: 100vh;
+    display: flex;
+    gap: 20px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--main);
+    overflow-x: hidden;
+    overflow-y: scroll;
+    transition: all 0.3s;
+    padding-bottom: 90px;
 `;
 const TitleNavbar = styled.div`
-  padding: 16px;
-  text-align: center;
-  width: 100%;
-  color: var(--white);
-  font-size: 24px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: normal;
-  border-bottom: 1px solid silver;
+    padding: 16px;
+    text-align: center;
+    width: 100%;
+    color: var(--white);
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    border-bottom: 1px solid silver;
 
-  -moz-user-select: none;
--khtml-user-select: none;
-user-select: none;   
+    -moz-user-select: none;
+    -khtml-user-select: none;
+    user-select: none;
 
-  cursor: pointer;
+    cursor: pointer;
 
-  :hover {
-    color: var(--secondary);
-    border-bottom: 1px solid var(--secondary);
-  }
-  :active{
-    background-color: #47474795;
-  }
-  transition: background-color 1s;
+    :hover {
+        color: var(--secondary);
+        border-bottom: 1px solid var(--secondary);
+    }
+    :active {
+        background-color: #47474795;
+    }
+    transition: background-color 1s;
 `;
 type PropsStyled = {
-  icon: string;
-  width: string;
-  height: string;
-  iconSize: string;
+    icon: string;
+    width: string;
+    height: string;
+    iconSize: string;
 };
 const Icon = styled.div`
-  min-width: ${({ width }: PropsStyled) => width};
-  min-height: ${({ height }: PropsStyled) => height};
-  background: url(${({ icon }: PropsStyled) => icon});
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: ${({ iconSize }: PropsStyled) => iconSize};
-  cursor: pointer;
-  border-radius: 50%;
+    min-width: ${({ width }: PropsStyled) => width};
+    min-height: ${({ height }: PropsStyled) => height};
+    background: url(${({ icon }: PropsStyled) => icon});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: ${({ iconSize }: PropsStyled) => iconSize};
+    cursor: pointer;
+    border-radius: 50%;
 
-  :active{
-    background-color: #47474795;
-  }
-  transition: all 1s;
+    :active {
+        background-color: #47474795;
+    }
+    transition: all 1s;
 `;
 const HeaderBurger = styled.div`
-  padding: 30px;
-  position: absolute;
-  top: 0;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
+    padding: 30px;
+    position: absolute;
+    top: 0;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
 `;
--- src/entities/MainHeader/components/MainHeader.tsx
+++ src/entities/MainHeader/components/MainHeader.tsx
@@ -7,165 +7,155 @@ import { Burger } from "./BurgerMenu";
 import logo from "../assets/logo.png";
 import search from "../assets/search.png";
 import burgerMenu from "../assets/burger.svg";
+import style from "../styles/Style.module.css";
+import { NavLink } from "react-router-dom";
 
 export const MainHeader = () => {
-  let navigate = useNavigate();
+    let navigate = useNavigate();
 
-  const [scrolled, setScrolled] = useState(false);
-  const [burger, setBurger] = useState<boolean>(false);
+    const [scrolled, setScrolled] = useState(false);
+    const [burger, setBurger] = useState<boolean>(false);
 
-  const handleBodyScroll = () => {
-    if (burger) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "auto";
-    }
-  };
-  
-  useEffect(() => {
-    handleBodyScroll();
-  }, [burger]);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const isScrolled = window.scrollY > 0;
-      setScrolled(isScrolled);
+    const handleBodyScroll = () => {
+        if (burger) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
     };
 
-    window.addEventListener("scroll", handleScroll);
+    useEffect(() => {
+        handleBodyScroll();
+    }, [burger]);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+    useEffect(() => {
+        const handleScroll = () => {
+            const isScrolled = window.scrollY > 0;
+            setScrolled(isScrolled);
+        };
 
-  return (
-    <HeaderContainer
-      background={scrolled}
-      header={
-        <Header>
-          <Icon
-            style={{display:'block'}}
-            width={"160px"}
-            height={"65px"}
-            iconSize={"160px"}
-            onClick={() => {
-              navigate("/");
-            }}
-            icon={logo}
-          />
-          <Navbar>
-            <TitleNavbar
-              onClick={() => {
-                navigate("/catalog");
-              }}
-            >
-              Каталог
-            </TitleNavbar>
-            <TitleNavbar
-              onClick={() => {
-                navigate("/bookmarks");
-              }}
-            >
-              Закладки
-            </TitleNavbar>
-            <TitleNavbar
-              onClick={() => {
-                navigate("/aboutUs");
-              }}
-            >
-              О нас
-            </TitleNavbar>
-          </Navbar>
-          <SecondaryBlock>
-            <Icon
-              width={"40px"}
-              height={"40px"}
-              iconSize={"40px"}
-              icon={search}
-              type={'true'}
-              onClick={() => {
-                navigate("/search");
-              }}
-            />
-            <Icon
-              width={"40px"}
-              height={"40px"}
-              iconSize={"40px"}
-              icon={burgerMenu}
-              onClick={()=>{setBurger(true)}}
-            />
-          </SecondaryBlock>
-          <Burger burger={burger} click={setBurger} /> 
-        </Header>
-      }
-    />
-  );
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return (
+        <HeaderContainer
+            background={scrolled}
+            header={
+                <Header>
+                    <Icon
+                        style={{ display: "block" }}
+                        width={"160px"}
+                        height={"65px"}
+                        iconSize={"160px"}
+                        onClick={() => {
+                            navigate("/");
+                        }}
+                        icon={logo}
+                    />
+                    <Navbar>
+                        <NavLink
+                            to={"/catalog"}
+                            className={({ isActive }) =>
+                                isActive ? style.ALink : style.link
+                            }
+                        >
+                            Каталог
+                        </NavLink>
+                        <NavLink
+                            to={"/bookmarks"}
+                            className={({ isActive }) =>
+                                isActive ? style.ALink : style.link
+                            }
+                        >
+                            Закладки
+                        </NavLink>
+                        <NavLink
+                            to={"/aboutUs"}
+                            className={({ isActive }) =>
+                                isActive ? style.ALink : style.link
+                            }
+                        >
+                            О нас
+                        </NavLink>
+                    </Navbar>
+                    <SecondaryBlock>
+                        <Icon
+                            width={"40px"}
+                            height={"40px"}
+                            iconSize={"40px"}
+                            icon={search}
+                            type={"true"}
+                            onClick={() => {
+                                navigate("/search");
+                            }}
+                        />
+                        <Icon
+                            width={"40px"}
+                            height={"40px"}
+                            iconSize={"40px"}
+                            icon={burgerMenu}
+                            onClick={() => {
+                                setBurger(true);
+                            }}
+                        />
+                    </SecondaryBlock>
+                    <Burger burger={burger} click={setBurger} />
+                </Header>
+            }
+        />
+    );
 };
 
 type PropsStyled = {
-  icon: string;
-  width: string;
-  height: string;
-  iconSize: string;
-  type?:string;
+    icon: string;
+    width: string;
+    height: string;
+    iconSize: string;
+    type?: string;
 };
 
 const Header = styled(Container)`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 `;
 const Icon = styled.div`
-  display: ${({ type }: PropsStyled) => type? 'block':'none'};
-  min-width: ${({ width }: PropsStyled) => width};
-  min-height: ${({ height }: PropsStyled) => height};
-  background: url(${({ icon }: PropsStyled) => icon});
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: ${({ iconSize }: PropsStyled) => iconSize};
-  cursor: pointer;
+    display: ${({ type }: PropsStyled) => (type ? "block" : "none")};
+    min-width: ${({ width }: PropsStyled) => width};
+    min-height: ${({ height }: PropsStyled) => height};
+    background: url(${({ icon }: PropsStyled) => icon});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: ${({ iconSize }: PropsStyled) => iconSize};
+    cursor: pointer;
 
-  @media (max-width: 700px) {
-    display: ${({ type }: PropsStyled) => type? 'none': 'block'};
-  }
+    @media (max-width: 700px) {
+        display: ${({ type }: PropsStyled) => (type ? "none" : "block")};
+    }
 `;
 
 const Navbar = styled.div`
-  display: flex;
-  width: 40%;
-  gap: 10px;
+    display: flex;
+    width: 40%;
+    gap: 10px;
 
-  align-items: center;
-  justify-content: space-around;
+    align-items: center;
+    justify-content: space-around;
 
-  font-style: normal;
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 18px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 18px;
 
-  @media (max-width: 700px) {
-    display: none;
-  }
-`;
-
-const TitleNavbar = styled.div`
-  color: var(--white);
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: normal;
-
-  :hover{
-    color:var(--secondary);
-  }
-
-  cursor: pointer;
-  -moz-user-select: none;
--khtml-user-select: none;
-user-select: none;   
+    @media (max-width: 700px) {
+        display: none;
+    }
 `;
 
 const SecondaryBlock = styled.div`
-  display: flex;
+    display: flex;
 `;
